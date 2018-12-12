2fa1f4dd4d2732700070b56e248cb092bd2a1e7a
NandoTheessen <nando.theessen@gmail.com>
2018-12-12 17:06:48 +0100

    Working solution for day2

--- 12/12.py
+++ 12/12.py
@@ -1,21 +1,20 @@
 lines = open('input.txt').read().split('\n')
+
 state = lines[0].split(': ')[1].strip()
 start_len = len(state)
-
 rules = {}
-
 for line in lines[2:]:
-    print(line)
-    before, after = line.split('=>')
-    rules[before.strip()] = after.strip()
+    if line:
+        before, after = line.split('=>')
+        rules[before.strip()] = after.strip()
 
-print(rules)
-print(0, state)
+# Important: ..... -> .
 zero_idx = 0
-for t in range(15000):
-    state = '..' + state + '..'
+print 0, state
+for t in xrange(15000):
+    state = '..'+state+'..'
     new_state = ['.' for _ in range(len(state))]
-    read_state = '..' + state + '..'
+    read_state = '..'+state+'..'
     zero_idx += 2
     for i in range(len(state)):
         pat = read_state[i:i+5]
@@ -29,13 +28,13 @@ for t in range(15000):
     while new_state[end] == '.':
         end -= 1
     state = ''.join(new_state[start:end+1])
-    print(t+1, zero_idx, state)
+    print t+1, zero_idx, state
 
-zero_idx = -int(50e9) + 45
+zero_idx = -int(50e9) + 34
 ans = 0
 for i in range(len(state)):
     if state[i] == '#':
-        ans += i - zero_idx
-        print(i-zero_idx, ans)
-
-print(ans)
+        ans += i-zero_idx
+        print i-zero_idx, ans
+print state, len(state), start_len
+print ans
